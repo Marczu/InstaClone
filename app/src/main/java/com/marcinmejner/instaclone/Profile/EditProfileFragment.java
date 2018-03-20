0a6ad1a2c26e02d4ad1b8f820147d64690ab6e25
Marcin Mejner <marczu09@gmail.com>
2018-03-20 11:19:09 +0100

    commit7 dodany Universal image loader

--- app/src/main/java/com/marcinmejner/instaclone/Profile/EditProfileFragment.java
+++ app/src/main/java/com/marcinmejner/instaclone/Profile/EditProfileFragment.java
@@ -6,8 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.marcinmejner.instaclone.R;
+import com.marcinmejner.instaclone.Utils.UniversalImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 /**
  * Created by Marc on 18.03.2018.
@@ -16,11 +19,39 @@ import com.marcinmejner.instaclone.R;
 public class EditProfileFragment extends Fragment {
     private static final String TAG = "EditProfileFragment";
 
+    private ImageView profileImage;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_editprofile, container, false);
+        profileImage = view.findViewById(R.id.profile_photo);
+
+        initImageLoader();
+
+        setProfileImage();
+
+        //ustawienie backarrow
+        ImageView backArrow = view.findViewById(R.id.backArrow);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().finish();
+
+            }
+        });
 
         return view;
+    }
+
+    private void initImageLoader(){
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(getActivity());
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
+
+    }
+
+    private void setProfileImage() {
+        String imageURL = "https://www.famousbirthdays.com/headshots/tori-amos-2.jpg";
+        UniversalImageLoader.setImage(imageURL, profileImage, null, "");
     }
 }
