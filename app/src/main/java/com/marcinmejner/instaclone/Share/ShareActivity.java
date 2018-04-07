faeae665df95b084cd3ba37f9b98cbee24f87545
Marcin Mejner <marczu09@gmail.com>
2018-04-07 02:08:18 +0200

    commit 29 Początek tworzenia ShareActivity dodanie weryfikacji pozwoleń

--- app/src/main/java/com/marcinmejner/instaclone/Share/ShareActivity.java
+++ app/src/main/java/com/marcinmejner/instaclone/Share/ShareActivity.java
@@ -1,8 +1,10 @@
 package com.marcinmejner.instaclone.Share;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -11,6 +13,7 @@ import android.view.MenuItem;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.marcinmejner.instaclone.R;
 import com.marcinmejner.instaclone.Utils.BottomNavigationViewHelper;
+import com.marcinmejner.instaclone.Utils.Permissions;
 
 /**
  * Created by Marc on 15.03.2018.
@@ -18,7 +21,10 @@ import com.marcinmejner.instaclone.Utils.BottomNavigationViewHelper;
 
 public class ShareActivity extends AppCompatActivity {
     private static final String TAG = "ShareActivity";
-    public static final int ACTIVITY_NUM = 2;
+
+    //Constants
+    private static final int ACTIVITY_NUM = 2;
+    public static final int VERIFY_PERMISSIONS_REQUEST = 1;
 
 
     private Context mContex = ShareActivity.this;
@@ -29,11 +35,66 @@ public class ShareActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Log.d(TAG, "onCreate: started");
 
-        setupNavigationNavigationView();
+        if (checkPermissionsArray(Permissions.PERMISSIONS)) {
+
+        } else {
+            veryfiPermissions(Permissions.PERMISSIONS);
+        }
+
+//        setupNavigationNavigationView();
     }
 
-    private void setupNavigationNavigationView(){
-        BottomNavigationViewEx bottomNavigationViewEx =  findViewById(R.id.bottomNavViewBar);
+    private void veryfiPermissions(String[] permissions) {
+        Log.d(TAG, "veryfiPermissions: weryfikujemy pozwolenia");
+
+        ActivityCompat.requestPermissions(
+                ShareActivity.this,
+                permissions,
+                VERIFY_PERMISSIONS_REQUEST
+        );
+    }
+
+    /**
+     * Sprawdzamy pozwolenia dla tablicy pozwoleń
+     *
+     * @param permissions
+     * @return
+     */
+    private boolean checkPermissionsArray(String[] permissions) {
+        Log.d(TAG, "checkPermissionsArray: sprawdzanie tablicy pozwoleń");
+
+        for (int i = 0; i < permissions.length; i++) {
+            String check = permissions[i];
+
+            if (!checkPermisions(check)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Sprawdzamy czy pojedyńcze pozwolenie zostało udzielone
+     *
+     * @param permission
+     * @return
+     */
+    private boolean checkPermisions(String permission) {
+        Log.d(TAG, "checkPermisions: sprawdzamy pozwolenie: " + permission);
+
+        int permissionRequest = ActivityCompat.checkSelfPermission(ShareActivity.this, permission);
+
+        if (permissionRequest != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "checkPermisions: Pozwolenie nie udzielone dla: " + permission);
+            return false;
+        } else {
+            Log.d(TAG, "checkPermisions: Pozwolenie zostało udzielone dla: " + permission);
+            return true;
+        }
+    }
+
+    private void setupNavigationNavigationView() {
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavViewBar);
         BottomNavigationViewHelper.setup(bottomNavigationViewEx);
         BottomNavigationViewHelper.enableNavigation(mContex, bottomNavigationViewEx);
         Menu menu = bottomNavigationViewEx.getMenu();
