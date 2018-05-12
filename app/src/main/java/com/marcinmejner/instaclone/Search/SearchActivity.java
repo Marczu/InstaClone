74781a757a1248a32784605aeef997cd52dd15e2
Marcin Mejner <marczu09@gmail.com>
2018-05-13 00:31:20 +0200

    commit 61 dodany adapter i działające wyszukiwanie po nazwie usera

--- app/src/main/java/com/marcinmejner/instaclone/Search/SearchActivity.java
+++ app/src/main/java/com/marcinmejner/instaclone/Search/SearchActivity.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -21,9 +24,12 @@ import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.marcinmejner.instaclone.R;
 import com.marcinmejner.instaclone.Utils.BottomNavigationViewHelper;
+import com.marcinmejner.instaclone.Utils.UserListAdapter;
 import com.marcinmejner.instaclone.models.User;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by Marc on 15.03.2018.
@@ -42,16 +48,44 @@ public class SearchActivity extends AppCompatActivity {
 
     //vars
     private List<User> mUserList;
+    private UserListAdapter mAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         Log.d(TAG, "onCreate: started");
+        mSearchParam = findViewById(R.id.search);
+        mListView = findViewById(R.id.listView);
 
         hideSoftKeyboard();
-
         setupNavigationNavigationView();
+        initTextListener();
+    }
+
+    private void initTextListener(){
+        Log.d(TAG, "initTextListener: initializing");
+
+        mUserList = new ArrayList<>();
+
+        mSearchParam.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                String text = mSearchParam.getText().toString();
+                searchForMatch(text);
+            }
+        });
     }
 
     private void searchForMatch(String keyword){
@@ -74,6 +108,7 @@ public class SearchActivity extends AppCompatActivity {
 
                         mUserList.add(singleSnapshot.getValue(User.class));
                         //update user list view
+                        updateUserList();
                     }
                 }
 
@@ -83,6 +118,21 @@ public class SearchActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    private void updateUserList(){
+        Log.d(TAG, "updateUserList: updatning users list");
+        mAdapter = new UserListAdapter(SearchActivity.this, R.layout.layout_user_listitem, mUserList);
+        mListView.setAdapter(mAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick: selected user: " + mUserList.get(position).toString());
+
+                //navigate to user profile
+            }
+        });
     }
 
     private void hideSoftKeyboard(){
